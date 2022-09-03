c65703e66a533dffdf2e01c2c4bc10ac8ea93917
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 14:24:11 +0700

    add: db context extension

---
+++ src/TodoList.Api/Extensions/DbContextExtension.cs
@@ -0,0 +1,27 @@
+﻿namespace TodoList.Api.Extensions
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using TodoList.Data.Contexts.ApplicationDb;
+
+    public static class DbContextExtension
+    {
+        private const string ConnectionStringName = "DefaultConnection";
+
+        public static void AddCustomSqliteContext(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration
+                .GetConnectionString(ConnectionStringName);
+
+            services.AddDbContext<ApplicationDbContext>(
+                o => o.UseSqlite(connectionString));
+
+            services.BuildServiceProvider()
+                .GetService<ApplicationDbContext>()
+                !.Database
+                .EnsureCreated();
+        }
+    }
+}
