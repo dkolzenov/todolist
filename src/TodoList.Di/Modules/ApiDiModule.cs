d4c0a6ee7ee25303db01bc8c25dfdbb3a99b3648
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 19:30:37 +0700

    add: api di module

---
+++ src/TodoList.Di/Modules/ApiDiModule.cs
@@ -0,0 +1,54 @@
+﻿namespace TodoList.Di.Modules
+{
+    using System.Reflection;
+
+    using Autofac;
+
+    using Module = Autofac.Module;
+
+    public class ApiDiModule : Module
+    {
+        private const string ServiceName = "TodoList.Services";
+
+        private const string DataName = "TodoList.Data";
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assemblies = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .OrderByDescending(a => a.FullName)
+                .ToArray();
+
+            ServicesRegister(ref builder, assemblies);
+            RepositoriesRegister(ref builder, assemblies);
+        }
+
+        private static void ServicesRegister(
+            ref ContainerBuilder builder,
+            Assembly[] assemblies)
+        {
+            var servicesAssembly = assemblies.FirstOrDefault(
+                t => t.FullName!
+                    .ToLower()
+                    .Contains(ServiceName.ToLower()));
+
+            builder.RegisterAssemblyTypes(servicesAssembly!)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
+        }
+
+        private static void RepositoriesRegister(
+            ref ContainerBuilder builder,
+            Assembly[] assemblies)
+        {
+            var dataAssembly = assemblies.FirstOrDefault(
+                t => t.FullName!
+                    .ToLower()
+                    .Contains(DataName.ToLower()));
+
+            builder.RegisterAssemblyTypes(dataAssembly!)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+        }
+    }
+}
