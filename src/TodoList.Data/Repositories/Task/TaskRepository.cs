f271d42cc8472e409ca435a123d7c8f41356fb11
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 14:15:29 +0700

    add: task repository

---
+++ src/TodoList.Data/Repositories/Task/TaskRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoList.Data.Repositories.Task
+{
+    using TodoList.Data.Repositories.Interfaces;
+    using TodoList.Data.Repositories.Base;
+    using TodoList.Data.Entities.Task;
+    using TodoList.Data.Contexts.ApplicationDb;
+
+    public class TaskRepository : RepositoryBase<TaskEntity>, ITaskRepository
+    {
+        public TaskRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
