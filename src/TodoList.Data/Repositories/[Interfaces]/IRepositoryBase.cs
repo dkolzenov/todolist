3874260f0cc66a3cc7a85681030bdc2f148b651c
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 14:10:04 +0700

    add: repository base

---
+++ src/TodoList.Data/Repositories/[Interfaces]/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoList.Data.Repositories.Interfaces
+{
+    using System.Linq.Expressions;
+
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAllAsync();
+
+        Task<bool> AddAsync(TEntity entity);
+
+        Task<bool> UpdateAsync(TEntity entity);
+
+        Task<bool> RemoveAsync(TEntity entity);
+
+        Task<List<TEntity>> QueryAsync(
+            params Expression<Func<TEntity, bool>>[] predicates);
+    }
+}
