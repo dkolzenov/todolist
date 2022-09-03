65585db76e4d30f1b4fedaf68c7f6e1e0b099c77
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 14:29:56 +0700

    add: task model

---
+++ src/TodoList.Services.Models/Task/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Services.Models.Task
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
