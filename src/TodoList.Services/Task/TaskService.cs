3691183f9bdc1d5876185f861fddb7546b89161e
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 15:17:01 +0700

    add: task service

---
+++ src/TodoList.Services/Task/TaskService.cs
@@ -0,0 +1,88 @@
+﻿namespace TodoList.Services.Task
+{
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using TodoList.Services.Interfaces;
+    using TodoList.Services.Models.Task;
+    using TodoList.Data.Repositories.Interfaces;
+    using TodoList.Data.Entities.Task;
+
+    public class TaskService : ITaskService
+    {
+        private readonly IMapper _mapper;
+
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskService(IMapper mapper, ITaskRepository taskRepository)
+        {
+            _mapper = mapper;
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<List<TaskModel>> GetAllTasksAsync()
+        {
+            try
+            {
+                var result = await _taskRepository.GetAllAsync();
+
+                var tasks = _mapper.Map<List<TaskModel>>(result);
+
+                return tasks;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<List<TaskModel>>(ex);
+            }
+        }
+
+        public async Task<bool> AddTaskAsync(TaskModel taskModel)
+        {
+            try
+            {
+                var task = _mapper.Map<TaskEntity>(taskModel);
+
+                var result = await _taskRepository.AddAsync(task);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<bool>(ex);
+            }
+        }
+
+        public async Task<bool> UpdateTaskAsync(TaskModel taskModel)
+        {
+            try
+            {
+                var task = _mapper.Map<TaskEntity>(taskModel);
+
+                var result = await _taskRepository.UpdateAsync(task);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<bool>(ex);
+            }
+        }
+
+        public async Task<bool> RemoveTaskAsync(TaskModel taskModel)
+        {
+            try
+            {
+                var task = _mapper.Map<TaskEntity>(taskModel);
+
+                var result = await _taskRepository.RemoveAsync(task);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<bool>(ex);
+            }
+        }
+    }
+}
---
+++ src/TodoList.Services/[Interfaces]/ITaskService.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoList.Services.Interfaces
+{
+    using TodoList.Services.Models.Task;
+
+    public interface ITaskService
+    {
+        Task<List<TaskModel>> GetAllTasksAsync();
+
+        Task<bool> AddTaskAsync(TaskModel taskModel);
+
+        Task<bool> UpdateTaskAsync(TaskModel taskModel);
+
+        Task<bool> RemoveTaskAsync(TaskModel taskModel);
+    }
+}
