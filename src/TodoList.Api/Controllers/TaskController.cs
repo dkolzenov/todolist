623969f7d7f7acb46d9f174bbf6e2a1b6c6aa866
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 15:28:55 +0700

    add: task controller

--- src/TodoList.Api/Controllers/TaskController.cs
+++ src/TodoList.Api/Controllers/TaskController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace TodoList.Api.Controllers
+﻿namespace TodoList.Api.Controllers
 {
-    public class TaskController : Controller
+    using Microsoft.AspNetCore.Mvc;
+
+    using TodoList.Services.Interfaces;
+    using TodoList.Services.Models.Task;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
         {
-            return View();
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TaskModel>>> GetAllTasksAsync()
+        {
+            var tasks = await _taskService.GetAllTasksAsync();
+
+            return tasks;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> AddTaskAsync(TaskModel taskModel)
+        {
+            var result = await _taskService.AddTaskAsync(taskModel);
+
+            return result;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<bool>> UpdateTaskAsync(TaskModel taskModel)
+        {
+            var result = await _taskService.UpdateTaskAsync(taskModel);
+
+            return result;
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> RemoveTaskAsync(TaskModel taskModel)
+        {
+            var result = await _taskService.RemoveTaskAsync(taskModel);
+
+            return result;
         }
     }
 }
