8981dca99a8f0acc417976d25bf48303553918ce
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-03 13:53:44 +0700

    add: Task entity

---
+++ src/TodoList.Data/Entities/Task/TaskEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Data.Entities.Task
+{
+    public class TaskEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
