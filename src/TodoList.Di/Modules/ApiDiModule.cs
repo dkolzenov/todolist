2beec1be022fbfef7ee162cd346587a16fd90d6e
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-04 18:34:37 +0700

    update: api di module

--- src/TodoList.Di/Modules/ApiDiModule.cs
+++ src/TodoList.Di/Modules/ApiDiModule.cs
@@ -1,54 +1,23 @@
 ﻿namespace TodoList.Di.Modules
 {
-    using System.Reflection;
-
     using Autofac;
 
-    using Module = Autofac.Module;
+    using TodoList.Di.Extensions;
 
     public class ApiDiModule : Module
     {
-        private const string ServiceName = "TodoList.Services";
-
-        private const string DataName = "TodoList.Data";
-
         protected override void Load(ContainerBuilder builder)
         {
+            Data.AssemblyRunner.Run();
+            Services.AssemblyRunner.Run();
+
             var assemblies = AppDomain.CurrentDomain
                 .GetAssemblies()
                 .OrderByDescending(a => a.FullName)
                 .ToArray();
 
-            ServicesRegister(ref builder, assemblies);
-            RepositoriesRegister(ref builder, assemblies);
-        }
-
-        private static void ServicesRegister(
-            ref ContainerBuilder builder,
-            Assembly[] assemblies)
-        {
-            var servicesAssembly = assemblies.FirstOrDefault(
-                t => t.FullName!
-                    .ToLower()
-                    .Contains(ServiceName.ToLower()));
-
-            builder.RegisterAssemblyTypes(servicesAssembly!)
-                .Where(t => t.Name.EndsWith("Service"))
-                .AsImplementedInterfaces();
-        }
-
-        private static void RepositoriesRegister(
-            ref ContainerBuilder builder,
-            Assembly[] assemblies)
-        {
-            var dataAssembly = assemblies.FirstOrDefault(
-                t => t.FullName!
-                    .ToLower()
-                    .Contains(DataName.ToLower()));
-
-            builder.RegisterAssemblyTypes(dataAssembly!)
-                .Where(t => t.Name.EndsWith("Repository"))
-                .AsImplementedInterfaces();
+            builder.RegisterServices(assemblies);
+            builder.RegisterRepositories(assemblies);
         }
     }
 }
