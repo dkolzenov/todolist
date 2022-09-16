709cbfd935759d7b56116e225f30e78797762db6
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-16 20:27:50 +0700

    add: api client service

---
+++ client/src/services/apiClientService.js
@@ -0,0 +1,47 @@
+class ApiClientService {
+  async get(url) {
+    const configInitGet = {
+      method: 'GET'
+    }
+    const response = await fetch(url, configInitGet);
+
+    return response;
+  }
+
+  async post(url, datatab) {
+    const configInitPost = {
+      method: 'POST',
+      body: JSON.stringify(datatab),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+    const response = await fetch(url, configInitPost);
+
+    return response;
+  }
+
+  async put(url, datatab) {
+    const configInitPut = {
+      method: 'PUT',
+      body: JSON.stringify(datatab),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+    const response = await fetch(url, configInitPut);
+
+    return response;
+  }
+
+  async delete(url) {
+    const configInitDelete = {
+      method: 'DELETE'
+    }
+    const response = await fetch(url, configInitDelete);
+
+    return response;
+  }
+}
+
+export default ApiClientService;
