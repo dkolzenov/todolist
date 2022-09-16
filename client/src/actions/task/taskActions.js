2c61e3bb1463d0d95e757279fcba8de4b2b6918b
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-16 22:06:00 +0700

    reformat taskActions.js

--- client/src/actions/task/taskActions.js
+++ client/src/actions/task/taskActions.js
@@ -5,7 +5,7 @@ import {
   DELETE_TASK
 } from '../actionTypes';
 
-export const getAllTasks = () => ({type: GET_ALL_TASKS});
-export const addTask = (data) => ({type: ADD_TASK});
-export const putTask = (data) => ({type: PUT_TASK});
-export const deleteTask = (data) => ({type: DELETE_TASK}); 
+export const getAllTasks = () => ({ type: GET_ALL_TASKS });
+export const addTask = (data) => ({ type: ADD_TASK });
+export const putTask = (data) => ({ type: PUT_TASK });
+export const deleteTask = (data) => ({ type: DELETE_TASK });
